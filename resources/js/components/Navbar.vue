ffbb4f5022fd853d8743d0f02ee1bc4d8ab8d233
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 14:10:25 +0800

    Add active  class to home

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="bg-red-500 py-4">
       <ul class="flex justify-center space-x-4">
         <li>
-          <Link href="/" class="text-white hover:text-gray-200">Home</Link>
+          <Link href="/" class="text-white hover:text-gray-200" :class="{'bg-red-800 p-2 rounded':$page.url === '/'}">Home</Link>
         </li>
         <li>
           <Link href="/users" class="text-white hover:text-gray-200">Users</Link>
