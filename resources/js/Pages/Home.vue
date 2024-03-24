750801218bf060a3530f849f8c0d8129aa5b35bf
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 12:52:10 +0800

    Add link

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,4 +1,11 @@
 <template>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/users">Users</a></li>
+            <li> <a href="/seetings">Seetings</a></li>
+        </ul>
+    </nav>
     <div>
         <h1>Welcome there to home page</h1>
     </div>
