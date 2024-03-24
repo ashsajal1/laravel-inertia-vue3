516239d199d8c4a44a12e26cb00687ac7ad8befc
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 12:17:52 +0800

    Send name as props props

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        <h1>Welcome there!</h1>
+        <h1>Welcome there, <strong>{{ name }}</strong>!</h1>
     </div>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const { name } = defineProps(['name']);
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Welcome', [
+        "name" => "Sajal"
+    ]);
 });
