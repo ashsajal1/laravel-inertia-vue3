d4579e825bf5d81be04dd92dd771431326391599
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 12:49:41 +0800

    Add users and seetings page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Welcome there to home page</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        "name" => "Sajal",
-        "framework" => ['Larvel', 'Vue', 'Inertia']
-    ]);
+    return Inertia::render('Home');
+});
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+Route::get('/seetings', function () {
+    return Inertia::render('Seetings');
 });
