a7a8602246c38337f534f473a1d2edecff36cf24
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 13:02:13 +0800

    Add indicator progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,4 +11,17 @@ createInertiaApp({
       .use(plugin)
       .mount(el)
   },
+  progress: {
+    // The delay after which the progress bar will appear, in milliseconds...
+    delay: 250,
+
+    // The color of the progress bar...
+    color: 'red',
+
+    // Whether to include the default NProgress styles...
+    includeCSS: true,
+
+    // Whether the NProgress spinner will be shown...
+    showSpinner: false,
+  },
 })
