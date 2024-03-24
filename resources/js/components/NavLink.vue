c2d6c6320e732789367cb90e62bc6bddc7a10b36
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 14:28:00 +0800

    Create NavLink component and wrap all link inside it

---
+++ resources/js/components/NavLink.vue
@@ -0,0 +1,11 @@
+<template>
+    <Link href="/" class="text-white hover:text-gray-200" :class="{ 'bg-red-800 p-2 rounded': active }">
+    <slot />
+    </Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { defineProps } from 'vue';
+const { active } = defineProps(['active'])
+</script>
--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,23 +1,32 @@
 <template>
     <nav class="bg-red-500 py-4">
-      <ul class="flex justify-center space-x-4">
-        <li>
-          <Link href="/" class="text-white hover:text-gray-200" :class="{'bg-red-800 p-2 rounded':$page.component === 'Home'}">Home</Link>
-        </li>
-        <li>
-          <Link href="/users?foo=bar" class="text-white hover:text-gray-200" :class="{'bg-red-800 p-2 rounded':$page.component === 'Users'}">Users</Link>
-        </li>
-        <li>
-          <Link href="/settings" class="text-white hover:text-gray-200" :class="{'bg-red-800 p-2 rounded':$page.component === 'Settings'}">Settings</Link>
-        </li>
-        <li>
-          <Link href="/logout" method="post" as="button" class="text-white hover:text-gray-200" :data="{foo:'bar'}">Log out</Link>
-        </li>
-      </ul>
+        <ul class="flex justify-center space-x-4">
+            <li>
+                <NavLink href="/" :active="$page.component === 'Home'">
+                    Home
+                </NavLink>
+
+            </li>
+            <li>
+                <NavLink href="/users" :active="$page.component === 'Users'">
+                    Users
+                </NavLink>
+            </li>
+            <li>
+                <NavLink href="/settings" :active="$page.component === 'Settings'">
+                    Settings
+                </NavLink>
+            </li>
+            <li>
+                <Link href="/logout" method="post" as="button" class="text-white hover:text-gray-200"
+                    :data="{ foo: 'bar' }">Log out</Link>
+            </li>
+        </ul>
     </nav>
-  </template>
-  
-  <script setup>
-  import { Link } from '@inertiajs/vue3'
-  </script>
-  
+</template>
+
+<script setup>
+import NavLink from './NavLink.vue';
+
+
+</script>
