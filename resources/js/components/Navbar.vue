babdfebff060c8f5107915e5e364659ca02c27c2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-24 12:56:36 +0800

    Change anchor tag with Link component of inertia vue3

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,9 +1,15 @@
 <template>
     <nav>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li> <a href="/seetings">Seetings</a></li>
+            <li>
+                <Link href="/">Home</ Link>
+            </li>
+            <li><Link href="/users">Users</Link></li>
+            <li> <Link href="/seetings">Seetings</Link></li>
         </ul>
     </nav>
 </template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
