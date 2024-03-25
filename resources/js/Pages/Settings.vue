db89d1304110002d315c20647ac631d0555f80f2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-25 12:41:26 +0800

    Use persistent layout

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,12 +1,14 @@
 <template>
-    <Layout>
-        <div>
-            <h1 class="text-3xl font-bold text-red-600">Welcome there to settings page</h1>
-        </div>
-    </Layout>
+    <div>
+        <h1 class="text-3xl font-bold text-red-600">Welcome there to settings page</h1>
+    </div>
 
 </template>
 
-<script setup>
+<script>
 import Layout from '../components/Layout.vue';
+
+export default {
+    layout: Layout,
+};
 </script>
