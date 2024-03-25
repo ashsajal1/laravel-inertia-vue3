848f9ae65ff67db24cd295c4db8772261135e1e3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-25 12:05:40 +0800

    access shared data using usePage instead $page.props

--- resources/js/components/Layout.vue
+++ resources/js/components/Layout.vue
@@ -1,9 +1,13 @@
 <template>
-  <Navbar></Navbar>
-  <p>Welcome back dear, {{ $page.props.auth.user.username }}!</p>
-  <slot />
+    <Navbar></Navbar>
+    <p>Welcome back dear, {{ username }}!</p>
+    <slot />
 </template>
 
 <script setup>
+import { usePage } from '@inertiajs/vue3';
 import Navbar from './Navbar.vue';
+import { computed } from 'vue';
+
+const username = computed(() => usePage().props.auth.user.username);
 </script>
